e6d450b17caf8c01aae3ff19b3b7e74ae0d652b2
bill1201 <bill19981201@gmail.com>
2018-01-16 14:25:17 +0800

    initial commit

---
+++ exam/src/edu/fju/exam/Niner.java
@@ -0,0 +1,17 @@
+package edu.fju.exam;
+
+public class Niner {
+
+	public static void main(String[] args) {
+		
+			for(int j=1;j<6;j++){
+				
+				for(int i=2;i<6;i++){
+				System.out.print(i+"x"+j+"="+(i*j));
+				System.out.print("\t");
+			}System.out.println("");
+		}
+
+	}
+
+}
